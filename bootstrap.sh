c650e5c87f829c611718451ba856311314e6a7e6
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-15 00:15:49 +0100

    No need for PLATFORM, support for non-Ubuntu is out of my scope.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,34 +5,31 @@ function log {
 }
 
 if [[ -z "$1" ]]; then
-  log "give me the name of your node buddy"
+  log "give me the name of your node buddy, exiting"
   exit 1
 fi
 
 if [ $OSTYPE = "linux-gnu" ]; then
-  OS="linux"
   if [ -x /usr/bin/apt-get ]; then
-    PLATFORM="ubuntu"
-    UBUNTU_CODENAME=`cat /etc/*-release | grep DISTRIB_CODENAME | sed 's/.*=//g'`
-    log "Ubuntu $UBUNTU_CODENAME Linux detected."
+    OS="ubuntu"
+  else
+    log "did not care to support, exiting"
+    exit 2
   fi
 else
   OS="osx"
-  PLATFORM="darwin"
 fi
 
-log "starting bootstrap for $PLATFORM"
-
-log "gaining superpowers"
+log "starting bootstrap for $OS"
 sudo -v
 
-if [ $PLATFORM = "ubuntu" ]; then
+if [ $OS = "ubuntu" ]; then
   log "Refreshing Apt database and getting initial dependencies"
   sudo apt-get -y -qq update
   sudo apt-get -y -qq install build-essential ruby rubygems
 fi
 
-if [ $PLATFORM = "darwin" ]; then
+if [ $OS = "osx" ]; then
   log "setting permissions on /usr/local for Homebrew"
   sudo chown -R `whoami`:staff /usr/local
 fi
