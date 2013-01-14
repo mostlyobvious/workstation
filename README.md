c9aac78ccc0eeb9eefc8b9b99610404f55bda36e
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-15 00:08:05 +0100

    It should actually work for Ubuntu and multiple nodes easily.

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # Workstation
 
-Workstation is my chef-solo setup for machine I work on. Inspired by [lunar station][lunar] and [Joshua Timberman][jtimberman] setup.
-Crafted lazily for OSX, but can be tweaked for other systems as well.
+Workstation is chef-solo setup for machines I work on. Inspired by [lunar station][lunar] and [Joshua Timberman][jtimberman] setup.
 
 [lunar]: https://github.com/LunarLogicPolska/lunar-station
 [jtimberman]: http://jtimberman.housepub.org/blog/2011/04/03/managing-my-workstations-with-chef/
 [roderik]: http://vanderveer.be/blog/2013/01/02/automating-the-setup-of-my-perfect-developer-environment-on-osx-10-dot-8-mountain-lion/
 
-## OSX Prerequirements
+## Usage
+
+Make yourself a node in `nodes/NAME.json` first, then:
+
+    ./bootstrap.sh NAME
+
+
+## OSX prerequirements
 
 [XCode][apple] or [Command Line Tools][apple].
 
+
 [apple]: https://developer.apple.com/downloads/index.action
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
 function log {
-  echo -e ">> $1"
+  echo -e ">> $1..."
 }
 
-log "Started bootstrap, hold on!"
+if [[ -z "$1" ]]; then
+  log "give me the name of your node buddy"
+  exit 1
+fi
 
-log "Getting up to date Rubygems, Chef and Librarian"
-sudo gem update --system
-sudo gem install chef
+if [ $OSTYPE = "linux-gnu" ]; then
+  OS="linux"
+  if [ -x /usr/bin/apt-get ]; then
+    PLATFORM="ubuntu"
+    UBUNTU_CODENAME=`cat /etc/*-release | grep DISTRIB_CODENAME | sed 's/.*=//g'`
+    log "Ubuntu $UBUNTU_CODENAME Linux detected."
+  fi
+else
+  OS="osx"
+  PLATFORM="darwin"
+fi
 
-log "Get all the cookbooks"
+log "starting bootstrap for $PLATFORM"
+
+log "gaining superpowers"
+sudo -v
+
+if [ $PLATFORM = "ubuntu" ]; then
+  log "Refreshing Apt database and getting initial dependencies"
+  sudo apt-get -y -qq update
+  sudo apt-get -y -qq install build-essential ruby rubygems
+fi
+
+if [ $PLATFORM = "darwin" ]; then
+  log "setting permissions on /usr/local for Homebrew"
+  sudo chown -R `whoami`:staff /usr/local
+fi
+
+log "getting Chef and Librarian"
+sudo gem install chef librarian
+
+log "bundling cookbooks"
 librarian-chef install
 
-log "Getting ownership of /usr/local"
-sudo chown -R `whoami`:staff /usr/local
+log "cooking with Chef"
+sudo chef-solo -c config/solo.rb -j nodes/$1.json
 
-log "Cooking with Chef"
-sudo chef-solo -c config/solo.rb -j nodes/osx.json
