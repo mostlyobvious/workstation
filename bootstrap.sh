9effa11db13a002861c8ca60814a1267bacde16b
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-16 22:43:22 +0100

    Don't set permissions if directory does not exist.

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,14 +24,16 @@ log "starting bootstrap for $OS"
 sudo -v
 
 if [ $OS = "ubuntu" ]; then
-  log "Refreshing Apt database and getting initial dependencies"
+  log "refreshing Apt database and getting initial dependencies"
   sudo apt-get -y -qq update
   sudo apt-get -y -qq install build-essential ruby rubygems
 fi
 
 if [ $OS = "osx" ]; then
-  log "setting permissions on /usr/local for Homebrew"
-  sudo chown -R `whoami`:staff /usr/local
+  if [[ -d /usr/local ]]; then
+    log "setting permissions on /usr/local for Homebrew"
+    sudo chown -R `whoami`:staff /usr/local
+  fi
 fi
 
 log "getting Chef and Librarian"
