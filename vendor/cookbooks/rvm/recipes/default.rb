760276612f5ea3bdc91f6707efee9cc2363c0731
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-17 21:13:50 +0100

    OSX rvm requirements.

--- vendor/cookbooks/rvm/recipes/default.rb
+++ vendor/cookbooks/rvm/recipes/default.rb
@@ -5,6 +5,23 @@ end
 
 execute "bash tmp/rvm-installer stable"
 
+if node[:platform] == "mac_os_x"
+  homebrew_tap "homebrew/dupes"
+
+  package "libksba"
+  package "autoconf"
+  package "automake"
+  package "apple-gcc42"
+
+  bash "rvm pkg install openssl" do
+    code <<-EOS
+      source "#{node[:rvm][:prefix]}/scripts/rvm"
+      rvm pkg install openssl
+    EOS
+  end
+end
+
+
 node[:rvm][:rubies].each do |ruby|
   bash "rvm install #{ruby}" do
     code <<-EOS
