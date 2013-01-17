c6580d1e86b4380b7c025cc622223740fbc6c6ba
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-17 20:49:00 +0100

    Included recently added cookbooks.

--- roles/base.rb
+++ roles/base.rb
@@ -2,6 +2,7 @@ description "Base apps."
 run_list [
   "recipe[vim]",
   "recipe[ctags]",
+  "recipe[bash::completion]",
   "recipe[firefox]",
   "recipe[google-chrome]"
 ]
--- roles/dev.rb
+++ roles/dev.rb
@@ -1,6 +1,7 @@
 description "General development cookbooks."
 run_list [
   "recipe[curl]",
+  "recipe[wget]",
   "recipe[ack]",
   "recipe[git]",
   "recipe[libxml2]"
