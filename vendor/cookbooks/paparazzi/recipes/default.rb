dfe6af4ca1fc37fd12bd9c24bc025f2d67b246d2
Paweł Pacana <pawel.pacana@gmail.com>
2013-01-19 18:00:16 +0100

    Fix typo.

--- vendor/cookbooks/paparazzi/recipes/default.rb
+++ vendor/cookbooks/paparazzi/recipes/default.rb
@@ -1,4 +1,4 @@
-dmg_package "Paparazzi" do
+dmg_package "Paparazzi!" do
   dmg_name    "Paparazzi!"
   volumes_dir "Paparazzi! 0.6.5"
   source "http://derailer.org/paparazzi/Paparazzi!%200.6.5.dmg"
