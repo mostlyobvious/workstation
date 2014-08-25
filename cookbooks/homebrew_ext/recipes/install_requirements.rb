6fe3e2abc498e7f8fbe5cb07ea4f15a2deaefcda
Paweł Pacana <pawel.pacana@gmail.com>
2014-08-25 18:25:09 +0200

    Side-effect from sudo chef run is moving homebrew requirements to cookbook.

---
+++ cookbooks/homebrew_ext/recipes/install_requirements.rb
@@ -0,0 +1,15 @@
+include_recipe 'homebrew'
+::Chef::Recipe.send(:include, Homebrew::Mixin)
+owner = homebrew_owner
+
+directory '/usr/local' do
+  owner owner
+  group 'staff'
+  recursive true
+end
+
+directory '/opt/homebrew-cask' do
+  owner owner
+  group 'staff'
+  recursive true
+end
